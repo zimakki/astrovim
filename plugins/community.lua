3e91088d78c715783b77a3170000024729af526c
Zi Makki <zimakki@gmail.com>
2023-05-20 21:15:35 +0200

    a couple more cool plugins I'm trying out

--- plugins/community.lua
+++ plugins/community.lua
@@ -7,6 +7,13 @@ return {
   { import = "astrocommunity.colorscheme.catppuccin" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.scrolling.mini-animate" },
+  -- TODO: try out spectra at some point
+  -- { import = "astrocommunity.project.nvim-spectra" },
+  -- TODO::work out why I can't jump to edit a file from lazygit - I thought flattn was supposed to fix this
+  -- { import = "astrocommunity.terminal-integration.flatten-nvim" },
+  { import = "astrocommunity.utility.noice-nvim" },
 }
