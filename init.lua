3e658670f816b27ad541c83fdc48b6c02340862f
Zi Makki <zimakki@gmail.com>
2023-06-12 18:35:34 +0200

    change the theme

--- init.lua
+++ init.lua
@@ -18,7 +18,7 @@ return {
   },
 
   -- Set colorscheme to use
-  colorscheme = "astrodark",
+  colorscheme = "tokyonight-moon",
 
   -- Diagnostics configuration (for vim.diagnostics.config({...})) when diagnostics are on
   diagnostics = {
---
+++ plugins/theme.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {},
+
+  require("notify").setup {
+    background_colour = "#000000",
+  },
+}
