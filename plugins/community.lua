3da5f04d90da52fa9aa50d2ea57db872fc226711
Zi Makki <zimakki@gmail.com>
2023-06-12 18:40:11 +0200

    some todo updates

--- plugins/community.lua
+++ plugins/community.lua
@@ -13,12 +13,12 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.editing-support.todo-comments-nvim" },
   { import = "astrocommunity.scrolling.mini-animate" },
-  -- TODO: try out spectra at some point
+  -- TODO: ok... so I have installed spectre but I don't know how to use it
   { import = "astrocommunity.project.nvim-spectre" },
-  -- TODO::work out why I can't jump to edit a file from lazygit - I thought flattn was supposed to fix this
-  -- { import = "astrocommunity.terminal-integration.flatten-nvim" },
   { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.motion.vim-matchup" },
+  -- TODO::work out why I can't jump to edit a file from lazygit - I thought flattn was supposed to fix this
+  -- UPDATE: so I can jump to edit a file from lazygit but it opens it in a popup window
   { import = "astrocommunity.terminal-integration.flatten-nvim" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   -- TODO:: see if there is a good plugin for Github PR's
