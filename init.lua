fb99dc8e2696918b350d703ad53d647cda1a860d
zi makki <zimakki@gmail.com>
2023-11-06 11:53:46 +0100

    update path of lexical.sh file

--- init.lua
+++ init.lua
@@ -1,16 +1,16 @@
 return {
   -- Configure AstroNvim updates
   updater = {
-    remote = "origin",     -- remote to use
-    channel = "stable",    -- "stable" or "nightly"
-    version = "latest",    -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-    branch = "nightly",    -- branch name (NIGHTLY ONLY)
-    commit = nil,          -- commit hash (NIGHTLY ONLY)
-    pin_plugins = nil,     -- nil, true, false (nil will pin plugins on stable only)
-    skip_prompts = false,  -- skip prompts about breaking changes
+    remote = "origin", -- remote to use
+    channel = "stable", -- "stable" or "nightly"
+    version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
+    branch = "nightly", -- branch name (NIGHTLY ONLY)
+    commit = nil, -- commit hash (NIGHTLY ONLY)
+    pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
+    skip_prompts = false, -- skip prompts about breaking changes
     show_changelog = true, -- show the changelog after performing an update
-    auto_quit = false,     -- automatically quit the current session after a successful update
-    remotes = {            -- easily add new remotes to track
+    auto_quit = false, -- automatically quit the current session after a successful update
+    remotes = { -- easily add new remotes to track
       --   ["remote_name"] = "https://remote_url.come/repo.git", -- full remote url
       --   ["remote2"] = "github_user/repo", -- GitHub user/repo shortcut,
       --   ["remote3"] = "github_user", -- GitHub user assume AstroNvim fork
@@ -31,7 +31,7 @@ return {
     formatting = {
       -- control auto formatting on save
       format_on_save = {
-        enabled = true,     -- enable or disable format on save globally
+        enabled = true, -- enable or disable format on save globally
         allow_filetypes = { -- enable format on save for specified filetypes only
           -- "go",
         },
@@ -56,13 +56,13 @@ return {
       lexical = function()
         return {
           cmd = {
-            "/Users/zimakki/code/lexical-lsp/lexical/_build/dev/rel/lexical/start_lexical.sh",
+            "/Users/zimakki/code/lexical-lsp/lexical/_build/dev/package/lexical/bin/start_lexical.sh",
           },
           filetypes = { "elixir", "eelixir", "heex", "surface" },
           root_dir = function(fname)
             local lspconfig = require "lspconfig"
             -- Set `~/Code/lexical` as root_dir for lexical project
-            local project = lspconfig.util.root_pattern ".git" (fname)
+            local project = lspconfig.util.root_pattern ".git"(fname)
             if project and string.sub(project, -12) == "code/lexical" then
               return project
             else
