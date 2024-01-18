9ec970e1d488e655ee30e3fd8e0d3848cf73da84
Zi Makki <zimakki@gmail.com>
2024-01-18 13:12:08 +0100

    make sure we have all the treesitters we need

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -5,7 +5,9 @@ return {
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
       "elixir",
       "heex",
-      "surface"
+      "eex",
+      "html",
+      "surface",
     })
   end,
 }
