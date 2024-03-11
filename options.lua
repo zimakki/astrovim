e54f4f802810ce5b8a23dd1633b7625c40698d86
Zi Makki <zimakki@gmail.com>
2024-03-11 10:42:03 +0100

    set cursorcolumn = true - better for pairprogramming

--- options.lua
+++ options.lua
@@ -7,6 +7,8 @@ return {
     spell = true, -- sets vim.opt.spell
     signcolumn = "auto", -- sets vim.opt.signcolumn to auto
     wrap = true, -- sets vim.opt.wrap
+    cursorcolumn = true, -- sets vim.opt.cursorcolumn
+    cursorline = true, -- sets vim.opt.cursorline
   },
   g = {
     mapleader = " ", -- sets vim.g.mapleader
