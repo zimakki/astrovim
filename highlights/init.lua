e1e3d6248644b59c0544bcbe1e269912bd5c6d1b
Zi Makki <zimakki@gmail.com>
2024-03-29 23:44:46 +0100

    make background of active window black! .. helps me focus on what window I'm on

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,12 +1,4 @@
 return {
   -- this table overrides highlights in all themes
-  -- Normal = { bg = "#000000" },
-  -- FloatBorder = {
-  --   bg = "#1e2030",
-  --   fg = "#589ed7",
-  -- },
-  -- LspFloatWinNormal = {
-  --   bg = "#1e2030",
-  -- },
-  -- NormalFloat = { bg = "#33354f" },
+  Normal = { bg = "#000000" },
 }
